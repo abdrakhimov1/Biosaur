c7c9d15b26305aa20b2bf69582cda0c280e3c6b9
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2019-10-29 12:31:04 +0300

    some setup staff

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ setup(
     description      = '''Proteomics post-search algorithm''',
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    author           = 'Mark Ivanov & Lev Levitsky & Julia Bubis',
+    author           = 'Abdrakhimov Daniil & Mark Ivanov',
     author_email     = 'pyteomics@googlegroups.com',
-    url              = 'https://bitbucket.org/markmipt/scavager',
+    url              = 'https://github.com/abdrakhimov1/Biosaur',
     packages         = ['biosaur_src', ],
     install_requires = [line.strip() for line in open('requirements.txt')],
     classifiers      = ['Intended Audience :: Science/Research',
                         'Programming Language :: Python :: 3',
                         'Topic :: Scientific/Engineering :: Bio-Informatics'],
-    license          = 'License :: OSI Approved :: Apache Software License',
+    license          = 'License :: OSI Approved :: GNU General Public License',
     entry_points     = {'console_scripts': ['biosaur = biosaur_src.biosaur:run']}
     )
