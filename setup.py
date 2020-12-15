fa07f0adb3c9cc7c62546e5c91a7de0a21e30a2a
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2020-12-15 13:24:28 +0300

    setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name             = 'Biosaur',
-    version          = '1.0.3',
+    version          = '2.0.0',
     description      = '''Proteomics post-search algorithm''',
     long_description = long_description,
     long_description_content_type = "text/markdown",
