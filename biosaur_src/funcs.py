bc848eeff65f961cba829aff842463caa7c5f5fa
abdrakhimov <dan.abdrakhimov@yandex.ru>
2021-02-16 11:58:29 +0300

    DIA

--- biosaur_src/funcs.py
+++ biosaur_src/funcs.py
@@ -108,8 +108,10 @@ def data_to_features(input_file, max_diff, min_length_hill, proccess_number, sta
         # break
         # pass
         k += 1
-    for i in ms2_data_for_analyse:
-        peak1.push_me_to_the_peak_nearest(i['m/z array'], i['intensity array'], max_diff, min_length_hill)
+
+    # for i in ms2_data_for_analyse:
+    #     peak1.push_me_to_the_peak_nearest(i['m/z array'], i['intensity array'], max_diff, min_length_hill)
+
     peak1.push_left(min_length=min_length_hill)
     # peak1.medar = np.array(peak1.medar)
     # peak1.medar = np.cumprod(peak1.medar)
